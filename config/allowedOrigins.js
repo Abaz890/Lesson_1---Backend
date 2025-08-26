54e2692a491ff6b166247a406009282e6a280b7d
Abdul Rehman <abaz77694@gmail.com>
2025-08-26 15:31:52 +0500

    Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://roledash.onrender.com/",
+  "https://roledash.onrender.com",
 ];
 
 module.exports = allowedOrigins;
